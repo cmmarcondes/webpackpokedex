5c313df394ea9534a4912284a5585fec7ce902c4
Carlos Magno Marcondes <you@example.com>
2020-10-18 21:34:58 -0300

    deploy

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
  const common = require('./webpack.config.js');
 
- module.exports = merge(commom, {
+ module.exports = merge(common, {
    mode: 'production',
  });
