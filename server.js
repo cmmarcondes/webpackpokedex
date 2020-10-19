563367ec42b34386d9883400e5436a2b17fca5d9
Carlos Magno Marcondes <you@example.com>
2020-10-19 01:14:36 -0300

    deploy

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const path = require('path');
 const express = require('express');
-const port = process.env.PORT;
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-process.env.PWD = process.cwd();
 
-app.use(express.static(process.env.PWD + '/dist'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/dist'));
 
-app.get('*', function (req, res) {
-  const index = path.join(process.env.PWD, '/public/index.html');
-  res.sendFile(index);
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.listen(port, '0.0.0.0')
-console.log('server_started');
+app.listen(port);
