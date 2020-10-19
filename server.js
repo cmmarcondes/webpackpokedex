b518fec893c0756d351b8bbf3a091a117219e047
Carlos Magno Marcondes <you@example.com>
2020-10-18 21:53:35 -0300

    deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
+const port = process.env.PORT;
 const app = express();
-app.set('port', (process.env.PORT || 5000));
 process.env.PWD = process.cwd();
 
 app.use(express.static(process.env.PWD + '/dist'));
@@ -11,12 +11,5 @@ app.get('*', function (req, res) {
   res.sendFile(index);
 });
 
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+app.listen(port, '0.0.0.0')
 console.log('server_started');
